6aaaa3235b7cda97249ebd237a0168549e3557b0
Momen <touristmomen@gmail.com>
2025-02-27 00:12:11 +0600

    My Works image hover effect added

--- src/Component/MyWorks.jsx
+++ src/Component/MyWorks.jsx
@@ -4,16 +4,37 @@ import myworks2 from "../assets/portfolio-img2.jpg";
 import myworks3 from "../assets/portfolio-img3.jpg";
 import myworks4 from "../assets/portfolio-img5.jpg";
 import myworks5 from "../assets/portfolio-img6.jpg";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 
 const MyWorks = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   const images = [
-    { src: myworks1, category: "Web Design" },
-    { src: myworks2, category: "Development" },
-    { src: myworks3, category: "Photography" },
-    { src: myworks4, category: "Web Design" },
-    { src: myworks5, category: "Development" },
+    {
+      src: myworks1,
+      category: "Web Design",
+      description: "When the music's over, turn off the light",
+    },
+    {
+      src: myworks2,
+      category: "Development",
+      description: "When the music's over, turn off the light",
+    },
+    {
+      src: myworks3,
+      category: "Photography",
+      description: "When the music's over, turn off the light",
+    },
+    {
+      src: myworks4,
+      category: "Web Design",
+      description: "When the music's over, turn off the light",
+    },
+    {
+      src: myworks5,
+      category: "Development",
+      description: "When the music's over, turn off the light",
+    },
   ];
 
   const filteredImages =
@@ -67,15 +88,26 @@ const MyWorks = () => {
           Photography
         </button>
       </div>
-
       <div className="columns-1 md:columns-2 lg:columns-3 gap-4">
         {filteredImages.map((image, index) => (
-          <div key={index} className="mb-4">
+          <div
+            key={index}
+            className="mb-4 relative group overflow-hidden rounded-lg shadow-lg"
+          >
             <img
               src={image.src}
               alt={`Gallery Image ${index + 1}`}
-              className="w-full rounded-lg shadow-lg"
+              className="w-full h-auto transition-transform duration-300 group-hover:scale-110"
             />
+            <div className="absolute inset-0 bg-[#9ecc54] bg-opacity-100 opacity-0 group-hover:opacity-40 transition-opacity duration-300 flex flex-col justify-end p-6">
+              <div className="self-end relative -top-50">
+                <FaArrowUpRightFromSquare className="text-white text-4xl opacity-100" />
+              </div>
+              <p className="text-white text-lg font-semibold mt-4 opacity-100">
+                {image.description}
+              </p>
+              <p className="text-white text-sm opacity-100">{image.category}</p>
+            </div>
           </div>
         ))}
       </div>
