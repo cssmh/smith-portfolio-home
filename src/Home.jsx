88ebabff40281567a448cd61b9dafc6eec04120c
Momen <touristmomen@gmail.com>
2025-02-27 11:21:22 +0600

    Achievements updated and exp to design

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import AboutMe from "./Component/AboutMe";
-import Achivements from "./Component/Achivements";
+import Achievements from "./Component/Achievements";
 import Count from "./Component/Count";
 import Header from "./Component/Header";
 import MyWorks from "./Component/MyWorks";
@@ -11,7 +11,7 @@ const Home = () => {
       <AboutMe />
       <Count />
       <MyWorks />
-      <Achivements />
+      <Achievements />
     </>
   );
 };
